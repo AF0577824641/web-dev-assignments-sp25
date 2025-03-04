55457c781c9d78ba98b15d6354a09b88f6965c94
O/I <65306934+AF0577824641@users.noreply.github.com>
2025-03-03 22:59:07 -0500

    Update genre.js

---
+++ week-6/models/genre.js
@@ -0,0 +1,15 @@
+const genres = [
+  {
+    name: "Science Fiction",
+    description:
+      "Fiction dealing with advanced technology, space exploration, and scientific concepts",
+  },
+  {
+    name: "Fantasy",
+    description: "Fiction with magical or supernatural elements",
+  },
+  {
+    name: "Mystery",
+    description: "Fiction centered around solving a crime or mysterious event",
+  },
+];
